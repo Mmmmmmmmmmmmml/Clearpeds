73b928c19fc004c5c40e7d1bf2f292e42d514ffe
Mmmmmmmmmmmmml <131415172+Mmmmmmmmmmmmml@users.noreply.github.com>
2025-01-06 00:29:43 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,13 +1,16 @@
-CreateThread(function()
-    while true do
-        ClearAreaOfPeds(169.8119, -968.3541, 29.6534, 40.0)
-        ClearAreaOfPeds(192.9869, -974.4473, 28.3004, 40.0)
-        -- Nehme die (--) raus, wenn du noch mehr Koordinaten eintragen willst
-        -- ClearAreaOfPeds(192.9869, -974.4473, 28.3004, 40.0)
-        
-        -- Nehme die (--) raus und stelle die Koordinaten ein, damit auch keine Fahrzeuge spawnen sollen
-        -- ClearAreaOfVehicles(1452.67, -2605.99, 48.52, 15.0, false, false, false, false, false)
-        -- ClearAreaOfVehicles(1452.67, -2605.99, 48.52, 15.0, false, false, false, false, false)
-        Wait(100)
+Citizen.CreateThread(function()
+    -- Wait 5 seconds after the server starts
+    Wait(5000)
+        -- Disable ped and scenario densities
+        SetPedDensityMultiplierThisFrame(0.0)
+        SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+
+        -- Replace x, y, z, and radius with your coordinates and radius for the MLO
+        local x, y, z = 100.0, 100.0, 100.0 -- Example coordinates
+        local radius = 50.0 -- Example radius
+
+        -- Clear peds from the area
+        ClearAreaOfPeds(x, y, z, radius, false)
+
     end
 end)
